a83717ccefa939896b17e2c84cbe061413d3ed4d
Purnendu-JU <163437589+Purnendu-JU@users.noreply.github.com>
2024-07-14 12:00:57 +0530

    Update Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -10,7 +10,7 @@ const Login = (props) => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const response = await fetch("https://news-app-backend-wine.vercel.app/api/auth/login", {
+    const response = await fetch("https://news-app-backend-red.vercel.app/api/auth/login", {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
